ebe4c0a89eaf0cdc54534e406598d20373f4f820
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2024-01-14 09:59:16 -0800

    Update Student.java

--- GroupProject/src/cs143/groupProject/Student.java
+++ GroupProject/src/cs143/groupProject/Student.java
@@ -1,5 +1,74 @@
 package cs143.groupProject;
+import java.util.*;
 
 public class Student extends Person{
+	//Giovanni Manabat
+	// member fields
 
+	 private int studentNum;
+
+
+	 private String major;
+
+
+	 public String type;
+
+	 // mutator methods
+
+	 public void setNumber(int studentNum) {
+
+	 this.studentNum = studentNum;
+
+	 }
+	 public void setMajor(String major) {
+
+	 this.major = major;
+
+	 }
+	 // accessor methods 
+
+	 public int getNumber() {
+
+	 return studentNum;
+
+	 }
+
+	 public String getMajor() {
+
+	 return major;
+
+	 }
+
+	 public String getType() {
+
+
+	 type = "Student";
+
+	 return type;
+
+	 }
+
+
+	 // print method
+
+	 public void printInfo() {
+
+
+	 System.out.println("-----------------------");
+
+
+	 System.out.println("Name: " + getName()); // for the name, we could add a set and get method of this into Person class
+
+
+	 System.out.println("Type: " + getType());
+
+
+	 System.out.println("Student Number: " + studentNum);
+
+
+	 System.out.println("Student Major: " + major);
+
+
+	 }
+	
 }
