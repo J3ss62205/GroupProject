343c6ee7052876c2dde73481b37b0c6b3eb1bea9
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2024-01-14 10:00:29 -0800

    Update UnderGrad.java

--- GroupProject/src/cs143/groupProject/UnderGrad.java
+++ GroupProject/src/cs143/groupProject/UnderGrad.java
@@ -1,5 +1,92 @@
 package cs143.groupProject;
 
-public class UnderGrad {
+	public class UnderGrad extends Student{
+		//Giovanni Manabat
 
-}
+	 // member field
+
+	 private String level;
+
+	 // mutator
+
+	 public void setLevel(String level) {
+
+	 this.level = level;
+
+	 }
+	 //allows student to reset as said in instructions(unsure if this is actually needed - level can be reset manually in main)
+
+	 /*
+
+	 public String resetLevel() {
+
+	 level = "Freshman";
+
+
+	 return level;
+
+
+	 } 
+
+
+	 */
+
+
+	 
+
+
+	 
+
+
+	 // accessor method
+
+
+	 public String getLevel() {
+
+
+	 return level;
+
+
+	 }
+
+
+	 
+
+
+	 @Override
+
+
+	 public String getType() {
+
+
+	 type = "Undergraduate";
+
+
+	 return type;
+
+
+	 }
+
+
+	 
+
+
+	 @Override
+
+
+	 public void printInfo() {
+
+
+	 super.printInfo();
+
+
+	 System.out.println("Student Level: " + level);
+
+
+	 }
+
+
+	 
+
+
+	}
