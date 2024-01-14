8adf122af0cb83c45b20a879dfefe4cf7f006a62
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2024-01-14 08:54:15 -0800

    Update Person.java

--- GroupProject/src/cs143/groupProject/Person.java
+++ GroupProject/src/cs143/groupProject/Person.java
@@ -1,5 +1,31 @@
 package cs143.groupProject;
 
-public class Person {
+// cs143 - Java 2
+//Jessica Henry
+// 1/14/23
 
+public class Person {
+	
+	private String name;
+	
+	public Person() {
+		name = "This person does not have a name";
+	}//This is the constructor for if we do not get a name
+	
+	public Person(String Name) {
+		name = Name;
+	}//This is the constructor for if we do get a name
+	
+	public void setName(String newName) {
+		name = newName;
+	}//This is the setter
+	
+	public String getName(){
+		return name;
+	}//This is the getter
+	
+	public void printInfo() {
+		System.out.println("Name: " + name);
+		System.out.println("Type: Person");
+	}//This is the print method in the Person class
 }
