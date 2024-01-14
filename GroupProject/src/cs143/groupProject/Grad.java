7a66c7075aec9dcadd56f1a34e092968ba6a3d69
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2024-01-14 10:01:02 -0800

    Update Grad.java

--- GroupProject/src/cs143/groupProject/Grad.java
+++ GroupProject/src/cs143/groupProject/Grad.java
@@ -1,5 +1,45 @@
 package cs143.groupProject;
 
-public class Grad {
+//Giovanni Manabat
+	public class Grad extends Student {
 
-}
+	 // member field
+	 private String degree;
+
+	 // mutator method
+
+	 public void setDegree(String degree) {
+
+	 this.degree = degree;
+
+	 }
+
+	 // accessor method
+
+	 public String getDegree() {
+	 return degree;
+	 }
+	 @Override
+	 public String getType() {
+
+	 type = "Graduate";
+	 return type;
+
+	 }
+
+	 //print method
+
+
+	 @Override
+
+
+	 public void printInfo() {
+
+
+	 super.printInfo();
+
+
+	 System.out.println("Student Degree: " + degree);
+
+	 }
+	}
